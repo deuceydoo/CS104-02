031f22f26a55d3beaddf5539b0a391a3ecbe93d4
deuceydoo <146123535+deuceydoo@users.noreply.github.com>
2023-10-04 21:48:39 -0400

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,21 @@
+# CS104
+
+# Deuce Lee
+
+# conditions.py
+
+temp = int(input("please enter the current temperature"))
+
+if temp > 90:
+    print("wear shorts")
+
+elif temp > 70:
+    print("short sleeves are fine")
+
+elif temp > 32:
+    print("wear a heavy coat")
+
+else:
+    print("STAY INSIDE")
+
+
